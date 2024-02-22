80c33fa93a78c74cb68cd0f4bf898f1de4255156
VineetShenvi <vinshenvi2004@gmail.com>
2024-02-22 18:11:54 +0530

    First Commit

---
+++ Experiment 3- Synchronization/NonSynchronized.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Server 
+{
+    private int availableSeats;
+    
+    Server(int availableSeats){
+        this.availableSeats = availableSeats;
+    }
+    
+	public void bookTicket(String pname) {
+	    System.out.println("Hi "+ pname + "!\nWelcome to Neeta Travels!");
+	    System.out.println("Number of seats available : "+availableSeats);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of tickets needed: ");
+        int n = sc.nextInt();
+		if ((availableSeats >= n) && (n > 0)) 
+        {
+			System.out.println("You have booked "+n+" seats.");
+			availableSeats = availableSeats- n;
+		} 
+		else 
+        System.out.println("Sorry, we do not have "+n+" tickets available!");
+		System.out.println();
+	}
+}
+
+class Passenger extends Thread 
+{
+	private Server s;
+	private String name;
+
+	public Passenger(Server s,String name) 
+    {
+		this.s = s;
+		this.name = name;
+	}
+	
+	public void run() 
+    {
+		s.bookTicket(name);
+	}
+}
+
+public class NonSynchronized 
+{
+    public static void main(String[] args) 
+    {
+        Server s = new Server(100);
+        Passenger t1 = new Passenger(s,"Taran");
+        Passenger t2 = new Passenger(s,"Vedant");
+        Passenger t3 = new Passenger(s, "Rushaan");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+
+
---
+++ Experiment 4- CPU Scheduling/SJF.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct processList 
+{
+ int n;
+ int AT;
+ int BT;
+ int WT;
+ int CT;
+};
+
+int SJF(int, struct processList *process);
+int sort(int, struct processList *process);
+
+int main()
+{
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    struct processList process[n];
+    printf("Enter arrival time and burst time of processes:\n");
+    for(int i=0; i<n; i++)
+    {
+        process[i].n = i;
+        printf("Process %d: ", i+1);
+        scanf("%d %d", &process[i].AT, &process[i].BT);
+    }
+    sort(n, process);
+    printf("\n\nProcesses you have enterred in sorted order based on burst time: \n"); 
+    printf("Processes   Arrival Time   Burst Time\n"); 
+    for (int  i=0; i<n; i++) 
+    { 
+        printf("\n  P%d ",process[i].n);
+        printf("            %d  ", process[i].AT );
+        printf("            %d ",process[i].BT );
+    }
+    SJF(n, process);
+}
+
+int SJF(int n, struct processList *process)
+{
+    for(int i=0; i<n; i++)
+    {
+        if (i==0)
+        {
+            process[0].WT = 0;
+        }
+        else
+        {
+            if(process[i].AT>process[i-1].CT)
+            {
+                process[i].WT = 0;
+            }
+            else
+            {
+                process[i].WT = process[i-1].CT-process[i].AT;
+            }
+        }
+        process[i].CT = process[i].AT + process[i].WT + process[i].BT;
+        
+    }
+        printf("\n\n Scheduled Processes: \n"); 
+        printf("Processes   Arrival Time   Burst Time   Wait Time   Completion Time\n"); 
+        for (int  i=0; i<n; i++) 
+        { 
+            printf("\n  P%d ",process[i].n);
+            printf("            %d  ", process[i].AT);
+            printf("            %d  ",process[i].BT );
+            printf("            %d  ",process[i].WT );
+            printf("            %d  ",process[i].CT );
+        }
+        
+}
+
+int sort(int n, struct processList *process)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (process[j].BT > process[j+1].BT) {
+                struct processList temp = process[j];
+                process[j] = process[j+1];
+                process[j+1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+            break;
+    }
+}
+
+
+
+
